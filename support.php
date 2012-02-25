898cb0e8af66e3b319f409b08c23c2ef077d4ea2
Jill Blevins <jill.blevins@gmail.com>
2012-02-25 10:23:41 -0500

    Basic Support Request Form
    Uses a class to generate the sticky form.
    Currently there is no error checking or action upon submission.

---
+++ support.php
@@ -0,0 +1,25 @@
+<?php
+require 'classes/form-class.php';
+$form = new Form('contact');
+
+$this->title = <<<_pageTitle_
+        WebCards: Support
+_pageTitle_;
+
+$this->content = <<<_pageContent_
+    <h3>Comments, Questions, Concerns</h3>
+	<p>Please send us your contact information and message. You will hear back from us soon.</p>
+
+	<form method="post" action="?p=support"><div>
+		{$form->generateFields('support')}
+		<input type="submit" class="sub" value="Send" />
+	</div></form>
+
+_pageContent_;
+
+$this->localNav = <<<_localNav_
+        
+_localNav_;
+?>
+
+
