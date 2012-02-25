05949a1c96693b81be16542b5630050b37e6f358
Brian McFarlane <brianmcfarlane@Brian-McFarlanes-MacBook-Pro.local>
2012-02-25 18:47:52 -0500

    Added more information to the features page.

--- features.php
+++ features.php
@@ -5,6 +5,14 @@ $this->title = <<<_pageTitle_
 _pageTitle_;
 
 $this->content = <<<_pageContent_
+        <div id="features-plans">
+                <h3>Bronze Plan</h3>
+                <p>Description of Bronze Plan.</p>
+                <h3>Silver Plan</h3>
+                <p>Description of Silver Plan.</p>
+                <h3>Gold Plan</h3>
+                <p>Description of Gold Plan.</p>
+        </div>
         <div id="features-themes">
                 <p>Choose between several different occassions for your WebCards.</p>
                 <h3>Theme 1</h3>
