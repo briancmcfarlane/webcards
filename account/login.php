c92f02cb5bc2f41fda9193cc6ea57cdfe2fce06e
Jill Blevins <jill.blevins@gmail.com>
2012-03-12 15:10:54 -0400

    revised login

--- account/login.php
+++ account/login.php
@@ -14,17 +14,18 @@ _pageTitle_;
 $this->content = <<<_pageContent_
 <h2>Login</h2>
 
+<p>Not a member? <a href="?p=signup-registration">Sign up</a> now</p>
+
 {$reg->logout_msg}
 {$reg->error_msg}
 
 <form method="post" action="?p=account-login">
 	<div>
 		<label for="emailadr">Email Address:</label><input type="text" size="20" name="email" id="emailadr" value="{$_POST['email']}" /><br />
-		<label for="passwd">Password:</label><input type="password" size="15" name="pwd" id="passwd" /><br /><br />
+		<label for="passwd">Password:</label><input type="password" size="20" name="pwd" id="passwd" /><br /><br />
 		<label>&nbsp;</label><input type="hidden" name="submitted" value="y" /><input type="submit" value="Login" /><br />
 	</div>
 </form>
-<p>Not a member? <a href="?p=signup-registration">Sign up</a> now</p>
 
 _pageContent_;
 
