2c2b40ee9e520e819bfb9d4bc138c6cd45b57eb3
Brian C. McFarlane <me@briancmcfarlane.com>
2012-03-18 14:55:50 -0400

    Rewrote registration to use the new plans and added form styles and heading styles.

--- signup/registration.php
+++ signup/registration.php
@@ -16,16 +16,15 @@ $this->content = <<<_pageContent_
             <label for="emailAddr">Email Address: </label><input type="text" size="20" name="email" id="emailAddr" value="{$_POST['email']}"/><br/>
             <label for="password">Password: </label><input type="password" size="20" name="password" id="password" /><br/>
             <label for="confirmpassword">Confirm Password: </label><input type="password" size="20" name="confirmpassword" id="confirmpassword" /><br/>
-            <label for="name">Name: </label><input type="text" size="20" name="name" id="name" value="{$_POST['name']}" /><br/>
-            <label for="ccnumber">Credit Card Number: </label><input type="text" size="20" name="ccnumber" id="ccnumber" value="{$_POST['ccnumber']}" />
-            <p>Choose your plan: <br/>
-                <label for="bronze">Bronze Plan </label><input type="radio" name="plan" id="bronze" value="Bronze" /> <br/>
-                <label for="silver">Silver Plan </label><input type="radio" name="plan" value="Silver" /> <br/>
-                <label for="gold">Gold Plan </label><input type="radio" name="plan" value="Gold" /> <br/>
-            </p>
+            <label for="name">Name: </label><input type="text" size="20" name="name" id="name" value="{$_POST['name']}" />
+            <h3>Choose Your Plan:</h3>
+                <label for="copper">Copper Plan</label><input type="radio" name="plan" value="Copper" id="copper"/><br />
+                <label for="silver">Silver Plan</label><input type="radio" name="plan" value="Silver" id="silver"/><br />
+                <label for="gold">Gold Plan</label><input type="radio" name="plan" value="Gold" id="gold"/><br />
+                <label for="platinum">Platinum Plan</label><input type="radio" name="plan" value="Platinum" id="platinum"/><br />
             <input type="hidden" name="submitted" value="y" />
-                                 
-            <input type="submit" value="Create Account"/><input type="reset" value="Reset Fields"/>
+            <br />                     
+            <label>&nbsp;</label><input type="submit" value="Create Account"/><input type="reset" value="Reset Fields"/>
         </div></form>
 _pageContent_;
 
