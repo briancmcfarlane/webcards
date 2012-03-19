42a05244bdd900790641a7020927dc21cf092196
Brian McFarlane <me@briancmcfarlane.com>
2012-03-18 20:33:00 -0400

    Updated features.php with new body copy

--- features.php
+++ features.php
@@ -9,22 +9,16 @@ _pageTitle_;
 
 $this->content = <<<_pageContent_
   <h2>Features</h2>
-  <p></p>
-        <div id="features-themes">
-                <p>Choose between several different occassions for your WebCards.</p>
-                <h3>Theme 1</h3>
-                <p>Description of theme 1</p>
-                <h3>Theme 2</h3>
-                <p>Description of theme 2</p>
-                <h3>Theme 3</h3>
-                <p>Description of theme 3</p>
-                <h3>Theme 4</h3>
-                <p>Description of theme 4</p>
-                <h3>Theme 5</h3>
-                <p>Description of theme 5</p>
-                <h3>Theme 6</h3>
-                <p>Description of theme 6</p>
-        </div>
+  <p>Send webcards to all your friends with this amazing service!</p>
+
+  <p>Choose between several different occassions for your WebCards:</p>
+    <ul>
+      <li>Dentist Appointment</li>
+      <li>Open House</li>
+      <li>Vet Appointment</li>
+      <li>Valentine's Day</li>
+    </ul>
+
 _pageContent_;
 
 $this->localNav = <<<_localNav_
